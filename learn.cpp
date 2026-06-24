74230bb2281b5eaee923cfd7bc476e43b70108c3
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-06-24 12:00:00 +0530

    feat: add competitive programming practice

---
+++ learn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hey, Striver! \n";
+    std::cout << "Hey, Striver!"<<"\n";
+    int x,y;
+    cin >> x;
+    cout << "Value of x: " << x<<"\n";
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y << "\n";
+    return 0;
+}
