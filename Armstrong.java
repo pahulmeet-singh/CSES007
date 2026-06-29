2d1044f13f04ff03c962a5aa6734bb021b482f59
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-06-29 14:35:22 +0530

    Armstrong

---
+++ Armstrong.java
@@ -0,0 +1,24 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int number = 153; // Example number
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+    }
+}
