751025690537a9182d4bcaafa3c9298f5802a235
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-07-02 09:02:47 +0530

    LC 73

--- setMatrixZeroes73.java
+++ setMatrixZeroes73.java
@@ -6,7 +6,59 @@ public class setMatrixZeroes73{
     }
     class Solution {
     public void setZeroes(int[][] matrix) {
-        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if the first row has any zeros
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has any zeros
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and column to mark zeros
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set the cells to zero based on the markers in the first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set the first row to zero if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set the first column to zero if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
 }
