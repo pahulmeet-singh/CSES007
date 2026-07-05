ceaa1aab6f39b3636ea1ffda6eedc2db768fd3aa
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-07-05 12:00:00 +0530

    feat: add Armstrong number solution

---
+++ Armstrong.java
@@ -0,0 +1,27 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int number = 153; // Example number
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+ 
+    public static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10; // get the last digit
+            sum += Math.pow(digit, digits); // add the digit raised to the power of the number of digits
+            num /= 10;
+        }
+
+        return sum == originalNum; 
+    }
+}
+// 2,90,62,026
+// https://docs.google.com/document/d/1zMBAQkaVJjgdSJ-AjlNM_IVmokbgHDzwXHZDKCKJQKM
+
