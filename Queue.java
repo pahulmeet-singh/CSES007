4d890481597eb06532e3c90cb784c35e1ebe8044
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-07-16 12:00:00 +0530

    feat: add queue implementation

---
+++ Queue.java
@@ -0,0 +1,77 @@
+public class Queue {
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    private Node front, rear;
+    private int size;
+
+    public Queue() {
+        front = rear = null;
+        size = 0;
+    }
+
+    // Enqueue operation
+    public void enqueue(int data) {
+        Node newNode = new Node(data);
+        if (rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    // Dequeue operation
+    public int dequeue() {
+        if (front == null) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        int data = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null; // If the queue becomes empty
+        }
+        size--;
+        return data;
+    }
+
+    // Display the queue
+    public void display() {
+        Node current = front;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    // Get the size of the queue
+    public int getSize() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        
+        System.out.println("Queue after enqueuing 10, 20, 30:");
+        queue.display();
+        
+        System.out.println("Dequeued: " + queue.dequeue());
+        
+        System.out.println("Queue after dequeuing:");
+        queue.display();
+        
+        System.out.println("Current size of the queue: " + queue.getSize());
+    }
+}
